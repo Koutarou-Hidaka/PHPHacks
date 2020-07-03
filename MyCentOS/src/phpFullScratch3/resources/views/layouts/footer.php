9225599a936fe1ed7a72090bbf7e2b173afde843
ミントッパ <mintppa@mintooa-pro.local>
2020-07-03 11:07:57 +0900

    ソース削除

--- MyCentOS/src/phpFullScratch3/resources/views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer bg-dark">
-    <div class="contaianer">
-        <p style="color:white; text-align:right;">
-            &copy; koutarou-hidaka All rights reserved   
-        </p>
-    </div>
-</footer>
--- MyCentOS/src/phpFullScratch3/resources/views/layouts/header.php
+++
@@ -1,9 +0,0 @@
-<header class="navbar navbar-dark bg-dark">
-    <div clsss="container">
-        <div>
-            <a class="navbar-brand" href="/">
-                PHP Full Sctach
-            </a>
-        </div>
-    </div>
-</header>
