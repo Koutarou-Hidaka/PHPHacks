db66677cd1c2e1bd2a2e635f97994e2b58fe4e4f
ミントッパ <mintppa@mintooa-pro.local>
2020-07-03 10:44:20 +0900

    practiceに追加

---
+++ MyCentOS/src/practice/app/Post.php
@@ -0,0 +1,153 @@
+<?php
+
+class Post {
+    private $DB_HOST="192.168.33.10";
+    private $DB_NAME="php_hacks";
+    private $DB_USER="root";
+    private $DB_PASSWORD="phpHacks2020@";
+
+    protected function db_access(){
+        // PHPがエラーを表示するように設定
+        // E_NOTICE 以外の全てのエラーを表示する
+        error_reporting(E_ALL & ~E_NOTICE);
+        
+        // データベースへの接続
+        try {
+            $dbh = new PDO('mysql:host='.$this->DB_HOST.';dbname='.$this->DB_NAME, $this->DB_USER, $this->DB_PASSWORD);
+            return $dbh;
+        } catch (PDOException $e) {
+            echo "エラー!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    protected function validation($data_title, $data_body){
+        $error = array();
+        
+        if (empty($data_title) || ctype_space($data_title)){//ctype_space・・・空白やタブなどの空文字
+            $error[] = "タイトルを入力してください。";
+        }
+        if (empty($data_body) || ctype_space($data_body)){//ctype_space・・・空白やタブなどの空文字
+            $error[] = "本文を入力してください。";
+        }
+        if (strlen($data_body) > 140){  //strlen・・・文字列の長さを数える
+            $error[] = "本文は140字以内で入力してください。";
+        }
+
+        return $error;
+    }
+
+    public function index(){
+        $dbh = $this->db_access();
+        // var_dump($dbh);die;
+        $sql = 'SELECT * FROM posts';
+        $stmt  = $dbh->prepare($sql);
+        // var_dump($stmt);die;
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // var_dump($result);die;
+        return $result;
+    }
+
+    public function store(){
+        $post = array(
+            "title" => $_POST['title'],
+            "body" => $_POST['body'],
+        );
+
+        $error = $this->validation($post['title'], $post['body']);
+
+        if (count($error)){
+            //createにエラーログを飛ばす
+        }else{
+
+            $dbh = $this->db_access();
+
+            try{
+                $dbh->beginTransaction();//以下の処理を一連の処理とまとめて、この間($dbh-comit()が完了するまで)にエラーが起きたらエラーを吐く
+                $sql = 'INSERT INTO posts(title, body) VALUES(:title, :body)';//:(title, :body)プレースフォルダ・・・実際の内容を後から挿入するために、とりあえず仮に確保した場所
+
+                $stmt  = $dbh->prepare($sql);
+                $stmt->bindValue(':title', $post['title'], PDO::PARAM_STR);
+                $stmt->bindValue(':body', $post['body'], PDO::PARAM_STR);
+                $stmt->execute();
+
+                $dbh->commit();
+            } catch(PDOException $Exception) {//引数・・・なんのエラーをキャッチするのか
+                $stmt->rollback();
+            }
+        }
+        
+        $result = array($post, $error);
+
+        return $result;
+    }
+
+    public function show($article_id){
+        $dbh = $this->db_access();
+      
+        $sql = 'SELECT * FROM posts WHERE id = :id';
+        $stmt  = $dbh->prepare($sql);
+        $stmt->bindValue(':id', $article_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function edit($article_id){
+        $dbh = $this->db_access();
+      
+        $sql = 'SELECT * FROM posts WHERE id = :id';
+        $stmt  = $dbh->prepare($sql);
+        $stmt->bindValue(':id', $article_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function update($article_id){
+        $dbh = $this->db_access();
+        // echo __LINE__."<br>";
+        // var_dump($article_id);die;
+        try{
+            $dbh->beginTransaction();//以下の処理を一連の処理とまとめて、この間($dbh-comit()が完了するまで)にエラーが起きたらエラーを吐く
+            
+            $sql = 'UPDATE posts SET title= :title, body = :body WHERE id= :id';//:(title, :body)プレースフォルダ・・・実際の内容を後から挿入するために、とりあえず仮に確保した場所
+
+            $stmt  = $dbh->prepare($sql);
+            $stmt->bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+            $stmt->bindValue(':body', $_POST['body'], PDO::PARAM_STR);
+            $stmt->bindValue(':id', $article_id, PDO::PARAM_INT);
+            $stmt->execute();
+
+            $dbh->commit();
+        } catch(PDOException $Exception) {//引数・・・なんのエラーをキャッチするのか
+            $stmt->rollback();
+        }
+
+        
+        $result = array($_POST['title'],$_POST['body']);
+
+        return $result;
+    }
+
+    public function destroy($article_id){
+        $dbh = $this->db_access();
+        // echo __LINE__;var_dump($article_id);die;
+        
+        $sql = 'DELETE  FROM posts WHERE id = :id';
+        $stmt  = $dbh->prepare($sql);
+        $stmt->bindValue(':id', $article_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+        // $result = $stmt->fetchAll(PDO::FETCH_ASSOC); 実行したら返す値がないため不要
+
+        return "OK";//なにも返ってこないので"ok"といれる
+    }
+
+
+}
---
+++ MyCentOS/src/practice/resources/views/posts/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <?php include($values["layouts"]."meta.php"); ?>
+</head>
+<body>
+    <?php include($values["layouts"]."header.php"); ?>
+
+    <main>
+        <div class= "container">
+            <div class="my-4">
+                <a class="btn btn-primary" href="/create" >
+                    投稿を新規作成する
+                </a>
+            </div>
+            <?php foreach($posts as $post):?>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h2><?php echo $post["title"];?></h1>
+                    </div>
+                    <div class="card-body">
+                        <p><?php echo $post["body"];?></p>
+
+                        <a class="card-link" href=<?php echo "show/".$post["id"];?>>
+                            詳細をみる
+                        </a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
+    <?php include($values["layouts"]."footer.php"); ?>
+
+
+
+    
+</body>
+</html>
